6bf8cfa814f88e7310641077c1a56d3604c66fed
bahrulrozak <bahrulrozak02@gmail.com>
2025-12-17 06:26:45 +0700

    config database using sequelize

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql2')
-const port = 3000
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+require("dotenv").config();
+const { Sequelize, DataTypes } = require("sequelize");
+const port = 3000;
 
-const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'love'
-})
 
-connection.connect((error) => {
-    if(error){
-        console.log("Gagal terhubung sayang", error)
-    }else{
-        console.log("Terhubung sayang...")
-    }
-})
+// Database connection
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Koneksi ke database BERHASIL!");
+  })
+  .catch((err) => {
+    console.log("Gagal terhubung ke database:", err.message);
+    // process.exit(1);
+  });
